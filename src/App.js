99d05cb7df9ae2e11f79000c2f70d269b0ffc996
Morgan Causey <morganelizabethcausey@gmail.com>
2023-10-04 17:11:02 -0400

    added toppings yup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Pizza from './components/Pizza';
+import Conformation from './components/Confirmation';
+
+const formSchema = yup.object().shape({
+  customer: yup
+    .string()
+    .required("Name must be entered")
+    .min(2, "name must be at least 2 characters"),
+  size: yup
+    .string()
+    .oneOf(["small", "medium", "large"], "Please select a size"),
+  sauce: yup
+    .string()
+    .required("please select a sauce"),
+  pepperoni: yup
+    .string(),
+  sausage: yup 
+    .string(),
+  bacon: yup
+    .string(),
+  ham: yup
+    .string(),
+  extraCheese: yup
+    .string(),
+  sub: yup
+    .string(),
+  special: yup
+    .string()
+    .notRequired(),
+  gluten: yup
+    .string()
+    .notRequired()
+})
+
+
 
 const App = () => {
   return (
