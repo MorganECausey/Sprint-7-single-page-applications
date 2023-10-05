ad52a596f0ef4bd471edf5866f599cb5161b1e50
Morgan Causey <morganelizabethcausey@gmail.com>
2023-10-05 17:54:52 -0400

    10/05 afternoon commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,4 +3,30 @@ import { Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
-    background-image: url`
+    background-image: url('');
+    background-repeat: no-repeat;
+    background-position: center;
+    color: white;
+    display: flex;
+    flex-direction: center;
+    padding: 10%;
+    border: double 8px red;
+    `;
+
+    function Home(props) {
+        return (
+            <div className="home-wrapper">
+                <header>
+                    <h1>Bloomtech Eats</h1>
+                    <div className="nav">
+                        <Link to="/"><button>Home</button></Link>
+                        <button>Help</button>
+                    </div>
+                </header>
+                <StyledWrapper className="buy-pizza">
+                    <h1>Your favorite food, delivered while coding</h1>
+                    <Link to="/pizza"><button id="order-pizza">Pizza!</button></Link>
+                </StyledWrapper>
+            </div>
+        )
+    }
