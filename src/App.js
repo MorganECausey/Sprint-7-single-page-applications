eeeb1fbb87dd3ea1824310f6c84909e7d5bb9032
Morgan Causey <morganelizabethcausey@gmail.com>
2023-10-05 08:22:07 -0400

    10/5 morning commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Conformation from './components/Confirmation';
 import axios from "axios";
 import * as yup from "yup";
 
+//create a form with required fields for validation like name, size, sauce, toppings etc
 const formSchema = yup.object().shape({
   customer: yup
     .string()
@@ -37,10 +38,31 @@ const formSchema = yup.object().shape({
     .notRequired()
 })
 
+//create inital form values for the form
 
 
+//create inital for value errors
+
+
+//create the inital disabled constant
+
+
+//create the inital states for the form values, disabled state, for errors, and orders
 const App = () => {
-  return (
+
+
+//create reset function
+
+//create a clear order function
+
+//create a calidation variable
+
+//create a state to update values as entered
+
+//create a state to post the orders to a new page
+
+//create a state to store order data 
+return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+    background-image: url`
